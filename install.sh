0755d20cd352479e116105c0cff8034f7412924f
Alex McLeod <amcleod1111@gmail.com>
2024-09-22 14:56:37 -0400

    Rearranged code to make sourcing dependent on successful symlink

--- install.sh
+++ install.sh
@@ -7,25 +7,19 @@ sudo apt update
 sudo apt-get update
 sudo snap set core experimental.refresh-app-awareness=true
 
-# Symlinks
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/.dotfiles/inputrc ~/etc/inputrc
-ln -sf ~/.dotfiles/.profile ~/.profile
+# Symlinks & Sourcing
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc && source ~/.bashrc
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases && source ~/.bash_aliases
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile && source ~/.bash_profile
+ln -sf ~/.dotfiles/inputrc ~/etc/inputrc && source ~/etc/inputrc
+ln -sf ~/.dotfiles/.profile ~/.profile && source ~/.profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-
-# Sourcing
-source ~/.bashrc
-source ~/.bash_aliases
-source ~/.bash_profile
-source ~/etc/inputrc
-source ~/.profile
 
 # Install Software
 sudo apt install neofetch
 sudo apt install xclip
 sudo apt install build-essential
+
 sudo snap install --classic nvim
 sudo snap install todoist
 sudo snap install --classic obsidian
